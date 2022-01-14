0f6031618e39cf0b1e706525ce27ecfac93517c6
alexmil-sk <alexmilweb@gmail.com>
2022-01-14 11:06:10 +0100

    start hw-1

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+const colors = require('colors/safe');
+
 const nums = process.argv.slice(2);
 const [num1, num2] = [nums[0], nums[1]];
 
+let i = +num1;
+const primeArr = [];
 
-var n = num2;
-
-for (var i = 2; i <= n; i++) {
-	var flag = 1;
+for (; i <= num2; i++) {
+	let flag = 1;
 	if (i > 2 && i % 2 != 0) {
-		for (var j = 3; j * j <= i; j = j + 2) {
+		for (let j = 3; j * j <= i; j = j + 2) {
 			if (i % j == 0) {
 				flag = 0;
 				break;
 			}
 		}
-	} else if (i != 2) flag = 0;
+	} else if (i != 2) {
+		flag = 0
+	};
 	if (flag == 1) {
-		console.log(i);
+		primeArr.push(i);
+		//console.log(i);
 	}
 }
+console.log(colors.magenta.bold(primeArr));
