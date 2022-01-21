251426d497f3a1f2b64921358c7cd8dc58bfeca4
alexmil-sk <alexmilweb@gmail.com>
2022-01-21 12:59:25 +0100

    hw-2 for control 3

--- Counter_argvs/start.js
+++ Counter_argvs/start.js
@@ -8,13 +8,11 @@ const myEmitter = new MyEmitter();
 
 Promise.resolve(process.argv)
 	.then(() => {
-		console.log(colors.bgGreen.black('Данные введены корректно...'));
-	})
-	.then(() => {
 		console.log('process.argv', process.argv)
 		const args = process.argv;
 		const [arg1, arg2, arg3, arg4, arg5, arg6] = [args[2], args[3], args[4], args[5], args[6], args[7]];
 		if (arg6) {
+			console.log(colors.bgGreen.black('Данные введены корректно...'));
 			console.log(colors.bgMagenta.black(`Ваши аргументы: Год ${arg1}, Месяц ${arg2}, День ${arg3}, Час ${arg4}, Минут ${arg5}, Секунд ${arg6}`));
 			console.log(colors.bgBlue.black('Обратный отсчет начался...'));
 			require('./counter.js');
